90fba7f1ec947cc675952302208a868ced3ce56e
haf-developer <projectxbug@gmail.com>
2017-01-07 13:04:01 +0200

    project start

---
+++ testsoundlib/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //Something to show that button was clicked.
+    ui->lineEdit->setText("Pushed");
+}
